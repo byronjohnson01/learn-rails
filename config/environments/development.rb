2d94d4f81017151ee5ff4f785820844f7ff6afdf
Byron Johnson <byron.johnson@gmail.com>
2014-12-08 21:32:03 -0700

    dynamic home page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,9 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-config.action_mailer.smtp_settings =
-	{ address: "smtp.gmail.com",
+config.action_view.raise_on_missing_translations = true
+config.action_mailer.smtp_settings = {
+	address: "smtp.gmail.com",
 	port: 587,
 	domain: Rails.application.secrets.domain_name,
 	authentication: "plain",
@@ -44,6 +44,7 @@ config.action_mailer.smtp_settings =
 	password: Rails.application.secrets.email_provider_password
 }
 # ActionMailer Config
-config.action_mailer.default_url_options = { :host = > 'localhost: 3000' }
-config.action_mailer.delivery_method = :smtp config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_url_options = { :host => 'localhost: 3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = true
 end
